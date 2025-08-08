21b435cc01e65e63992bd4d559651a41520edbdc
kalgetachew375 <kalgetachew375@gmail.com>
2025-08-08 18:25:00 +0000

    init commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+//RouterOutlet,
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
